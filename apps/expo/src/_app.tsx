29cf5240e2d3c519639175f2298629beb3c16de5
Dalton Kye Miller <36522884+daltonkyemiller@users.noreply.github.com>
2022-12-30 19:38:07 -0600

    Initial commit

---
+++ apps/expo/src/_app.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { TRPCProvider } from "./utils/trpc";
+
+import { HomeScreen } from "./screens/home";
+import { SignInSignUpScreen } from "./screens/signin";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import { tokenCache } from "./utils/cache";
+
+// Find this in your Dashboard.
+const clerk_frontend_api = "YOUR_CLERK_FRONTEND_API";
+
+export const App = () => {
+  return (
+    <ClerkProvider frontendApi={clerk_frontend_api} tokenCache={tokenCache}>
+      <SignedIn>
+        <TRPCProvider>
+          <SafeAreaProvider>
+            <HomeScreen />
+            <StatusBar />
+          </SafeAreaProvider>
+        </TRPCProvider>
+      </SignedIn>
+      <SignedOut>
+        <SignInSignUpScreen />
+      </SignedOut>
+    </ClerkProvider>
+  );
+};
