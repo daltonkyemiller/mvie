847ede6b9c3d6d108086981d1231c04404a68b81
Dalton Kye Miller <36522884+daltonkyemiller@users.noreply.github.com>
2023-01-05 22:27:38 -0600

    cool landing animation

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -7,8 +7,9 @@ import { useAuth, UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import clsx from 'clsx';
-import { FC, useMemo } from 'react';
-import { motion, Variants } from 'framer-motion';
+import { FC, useEffect, useMemo, useState } from 'react';
+import { motion, MotionConfig, TargetAndTransition, useAnimationControls, useScroll, Variants } from 'framer-motion';
+import { random } from 'lodash';
 
 const PostCard: FC<{
   post: inferProcedureOutput<AppRouter['post']['all']>[number];
@@ -24,7 +25,7 @@ const PostCard: FC<{
 const Home: NextPage = () => {
   const imgProps = useMemo(() => ({ fill: true, className: 'h-full w-full object-cover' }), []);
   const imgContainerProps = useMemo(
-    () => ({ className: 'relative aspect-[4/3] w-[200px] overflow-hidden rounded-xl' }),
+    () => ({ className: 'relative aspect-[4/3] w-[300px] overflow-hidden rounded-xl' }),
     [],
   );
 
@@ -36,14 +37,47 @@ const Home: NextPage = () => {
     'virgin_suicides.jpeg',
   ] as const;
 
-  const centerVariants: Variants = {
-    start: { opacity: 1, scale: 5 },
-    in: {
-      opacity: 1,
-      scale: 1,
+  const containerControls = useAnimationControls();
+  const centerControls = useAnimationControls();
+  const leftSideControls = useAnimationControls();
+  const rightSideControls = useAnimationControls();
+  const headerControls = useAnimationControls();
+  const individualImageControls = useAnimationControls();
+
+  const floatAround: TargetAndTransition = {
+    transition: {
+      duration: 10,
+      repeat: Infinity,
+      repeatType: 'mirror',
     },
-    out: { opacity: 0 },
   };
+
+  const sequence = async () => {
+    containerControls.set({ y: -100 });
+    leftSideControls.set({ opacity: 0 });
+    rightSideControls.set({ opacity: 0 });
+    await individualImageControls.start((i) => ({
+      scale: [0, 1],
+      transition: {
+        delay: (i ?? 1) * random(1, 0.5),
+        type: 'spring',
+        damping: 10,
+        duration: 0.35,
+      },
+    }));
+    leftSideControls.start({ opacity: 1, transition: { duration: 0.25 } });
+    rightSideControls.start({ opacity: 1, transition: { duration: 0.25 } });
+    await containerControls.start({
+      y: [-100, 0],
+    });
+    headerControls.start({
+      opacity: [0, 1],
+    });
+  };
+
+  useEffect(() => {
+    sequence();
+  }, [sequence]);
 
   return (
     <>
@@ -51,30 +85,65 @@ const Home: NextPage = () => {
         <title>Mvie</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="flex h-full w-full items-center justify-center">
-        <div className="grid grid-flow-col grid-cols-3 items-center justify-items-center gap-10">
-          <motion.div className="flex flex-col gap-5">
-            <div {...imgContainerProps}>
-              <Image src={`/img/${images[0]}`} alt={images[0]} {...imgProps} />
+      <section className="flex h-full w-full flex-col items-center justify-center gap-10 p-10">
+        <motion.header className="self-start" animate={headerControls} initial={{ opacity: 0 }}>
+          <h1 className="font-brand font-display text-9xl">mvie</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, quaerat.</p>
+        </motion.header>
+        <MotionConfig transition={{ duration: 2 }}>
+          <motion.div className="flex items-center justify-center gap-10" animate={containerControls}>
+            {/*Far Left Side*/}
+            <motion.div animate={leftSideControls}>
+              <div {...imgContainerProps} className={clsx(imgContainerProps.className, '!w-[300px]')}>
+                <Image src={`/img/${images[2]}`} alt={images[2]} {...imgProps} />
+              </div>
+            </motion.div>
+
+            {/*Left Side*/}
+            <div className="flex flex-col items-center justify-center gap-5">
+              <motion.div
+                {...imgContainerProps}
+                className={clsx(imgContainerProps.className, `!aspect-[3/4] !w-[200px]`)}
+                animate={individualImageControls}>
+                <Image src={`/img/${images[0]}`} alt={images[0]} {...imgProps} />
+              </motion.div>
+              <motion.div {...imgContainerProps} animate={individualImageControls}>
+                <Image src={`/img/${images[1]}`} alt={images[1]} {...imgProps} />
+              </motion.div>
             </div>
-            <div {...imgContainerProps}>
-              <Image src={`/img/${images[1]}`} alt={images[1]} {...imgProps} />
+
+            {/*Center */}
+            <motion.div animate={centerControls}>
+              <motion.div
+                {...imgContainerProps}
+                className={clsx(imgContainerProps.className, 'w-[400px]')}
+                animate={individualImageControls}
+                custom={0}>
+                <Image src={`/img/${images[2]}`} alt={images[2]} {...imgProps} />
+              </motion.div>
+            </motion.div>
+
+            {/*Right side*/}
+            <div className="flex flex-col items-center justify-center gap-5">
+              <motion.div {...imgContainerProps} animate={individualImageControls}>
+                <Image src={`/img/${images[3]}`} alt={images[3]} {...imgProps} />
+              </motion.div>
+              <motion.div
+                {...imgContainerProps}
+                className={clsx(imgContainerProps.className, '!aspect-square w-[200px]')}
+                animate={individualImageControls}>
+                <Image src={`/img/${images[4]}`} alt={images[4]} {...imgProps} />
+              </motion.div>
             </div>
+
+            {/*Far Right side*/}
+            <motion.div animate={rightSideControls}>
+              <div {...imgContainerProps} className={clsx(imgContainerProps.className, '!w-[300px]')}>
+                <Image src={`/img/${images[2]}`} alt={images[2]} {...imgProps} />
+              </div>
+            </motion.div>
           </motion.div>
-          <motion.div variants={centerVariants} initial="start" animate="in" exit="out">
-            <div {...imgContainerProps} className={clsx(imgContainerProps.className, 'w-[400px]')}>
-              <Image src={`/img/${images[2]}`} alt={images[2]} {...imgProps} />
-            </div>
-          </motion.div>
-          <div className="flex flex-col gap-5">
-            <div {...imgContainerProps}>
-              <Image src={`/img/${images[3]}`} alt={images[3]} {...imgProps} />
-            </div>
-            <div {...imgContainerProps}>
-              <Image src={`/img/${images[4]}`} alt={images[4]} {...imgProps} />
-            </div>
-          </div>
-        </div>
+        </MotionConfig>
       </section>
     </>
   );
